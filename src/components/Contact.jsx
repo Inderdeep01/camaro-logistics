b8ae6b58eaded00ac3fbf7941e7e5d645da78733
inderdeep01 <inderdeepsidhu01@gmail.com>
2024-11-07 23:49:13 +0530

    adding more images and changing email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ function Contact(){
         <div id='contact_container'>
             <div id='contact_us'>Contact Us</div>
             <div id='contact_info'>
-                <div>Email: <b>admin@camarologistics.com</b></div>
+                <div>Email: <b>accounts@camarologistics.com</b></div>
                 <div>Location: 177 Martinvalley Crescent NE, Calgary,<br/> AB <i>T3J 4L6</i>, Canada</div>
             </div>
             <div id='request_callback'>
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,6 +4,7 @@ import img_2 from "../assets/img_2.png"
 import img_3 from "../assets/img_3.png"
 import img_4 from "../assets/img_4.png"
 import img_5 from "../assets/img_5.png"
+import img_6 from "../assets/img_7.png"
 function Gallery(props){
     return(
         <>
@@ -16,6 +17,7 @@ function Gallery(props){
             <div className='gallery_box' id='box_3'><img className='gallery_img' src={img_3}/></div>
             <div className='gallery_box' id='box_4'><img className='gallery_img' src={img_4}/></div>
             <div className='gallery_box' id='box_5'><img className='gallery_img' src={img_5}/></div>
+            <div className='gallery_box' id='box_6'><img className='gallery_img' src={img_6}/></div>
         </div>
         </>
     )
